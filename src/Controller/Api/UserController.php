e98e396e7d550ff1b1aaa7fc89903e1ace8908a0
Quach <kevin.quach@outlook.fr>
2018-08-21 16:24:16 +0200

    remove car controller

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -77,6 +77,7 @@ class UserController extends FOSRestBundle
      * )
      *
      * @param Request $request
+     * @return User|JsonResponse
      */
     public function createUser(Request $request)
     {
